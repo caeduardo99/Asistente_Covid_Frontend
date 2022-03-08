02777b49c72cfd58c71295bd3051eb853cbf4fbd
Carlos Soria <carlossoria1999@gmail.com>
2022-03-08 14:50:13 -0500

    Creation labels and imputs, Validations email and password fields

--- covid_frontend/src/app/login/login.page.ts
+++ covid_frontend/src/app/login/login.page.ts
@@ -1,15 +1,54 @@
 import { Component, OnInit } from '@angular/core';
+import { 
+  FormBuilder, 
+  FormControl, 
+  FormGroup, 
+  Validators 
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+  styleUrls: ['./login.page.scss']
 })
 export class LoginPage implements OnInit {
-
-  constructor() { }
-
+  loginForm:FormGroup;
+  validationMessages = {
+    email: [
+      {
+        type: 'required', message: 'El email es requerido'
+      },
+      {
+        type: 'pattern', message: 'El email es incorrecto'
+      }],
+      password: [
+        {
+          type: 'required', message: 'El password es requerido'
+        },
+        {
+          type: 'minLength', message: 'Tamaño minimo 5 caracteres'
+        }],
+     };
+     
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      email: new FormControl(
+        "",
+        Validators.compose([
+        Validators.required,
+        Validators.pattern("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$")
+      ])
+      ),
+      password: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(8)
+      ])),
+    })
+   }
+  
   ngOnInit() {
-  }
-
+   }
+   loginUser(credentials){
+     console.log(credentials);
+   }
 }
