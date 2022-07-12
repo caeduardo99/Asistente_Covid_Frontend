38ff1d05f398b314a7f8c300283fa3e2770fc61f
Carlos Soria <carlossoria1999@gmail.com>
2022-07-12 12:48:33 -0500

    Creation of component table for edit users

--- covid_frontend/src/app/admin/admin.page.ts
+++ covid_frontend/src/app/admin/admin.page.ts
@@ -1,23 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthenticateService } from '../services/authenticate.service';
-
+import { Component, OnInit } from "@angular/core";
+import { AuthenticateService } from "../services/authenticate.service";
+import { Router } from "@angular/router";
+import { Usuario } from "../model/Usuario";
 @Component({
-  selector: 'app-admin',
-  templateUrl: './admin.page.html',
-  styleUrls: ['./admin.page.scss'],
+    selector: "app-admin",
+    templateUrl: "./admin.page.html",
+    styleUrls: ["./admin.page.scss"],
 })
 export class AdminPage implements OnInit {
-  admin:any; 
-  constructor(private authService: AuthenticateService) { }
+    admin: any;
+    private _usuario: Usuario;
+    constructor(
+        private authService: AuthenticateService,
+        private router: Router
+    ) {}
 
-  ngOnInit() {
-    this.getPosts();
-  }
+    ngOnInit() {
+        this.getPosts();
+    }
 
-  getPosts() { //llamamos a la funcion getPost de nuestro servicio.
-    this.authService.getAdmin()
-    .then(data => {
-      this.admin = data;
-    });
-}
+    getPosts() {
+        //llamamos a la funcion getPost de nuestro servicio.
+        this.authService.getAdmin().then((data) => {
+            //   console.log(data);
+            this.admin = data;
+        });
+    }
+
+    editarUsuario(id) {
+        console.log(id);
+    }
 }
--- covid_frontend/src/app/model/Usuario.ts
+++ covid_frontend/src/app/model/Usuario.ts
@@ -1,15 +1,15 @@
-export class Usuario{
-    id:number
-    cedula: string
-    nombre: string
-    apellido: string
-    institucion: string
-    direccion: string
-    telefono: number
-    email: string
-    password: string
-    latitud: number
-    longitud: number
-    roles: string[] = [];
-
+export class Usuario {
+  id: number;
+  cedula: string;
+  nombre: string;
+  apellido: string;
+  institucion: string;
+  direccion: string;
+  telefono: number;
+  email: string;
+  password: string;
+  latitud: number;
+  longitud: number;
+  estado: boolean;
+  roles: string[] = [];
 }
